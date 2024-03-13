0391c17a494cd5ebe67dad0b4ce9de72455a0c84
Rohanpatel512 <rohanpat223@gmail.com>
2024-03-13 12:41:44 -0400

    Created Invetory class file.

---
+++ src/Inventory.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Inventory {
+
+    // Fields
+    private int supplierID;
+    private int quantity;
+    private double price;
+    private String productName;
+    private String status;
+    private String supplierName;
+
+    /**
+     * Constructor for the inventory class
+     */
+    public Inventory() {
+
+    }
+
+    /**
+     * Combines the columns that will be placed in formated file.
+     * @returns an array of all the columns that were combined
+     */
+    private ArrayList<Integer> combineColumns() {
+
+        return null;
+        
+    }  
+
+    /**
+     * Places all columns into a file
+     */
+    private void columnsToFile() {
+
+
+    }   
+
+}
