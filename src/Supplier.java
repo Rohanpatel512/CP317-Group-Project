0a861cb8f06bdbc8bf2a4476c48be85444497b07
dn-ngo <158779546+dn-ngo@users.noreply.github.com>
2024-03-15 07:40:20 -0400

    add getters and setter for Supplier

--- src/Supplier.java
+++ src/Supplier.java
@@ -1,29 +1,60 @@
 public class Supplier {
-  //Attributes
-  private int supplierID;
-  private String supplierName;
+    // Private vars from Supplier Documentation
+    private int supplierID;
+    private String supplierName;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
 
-  //Constructor
-  public Supplier(int supplierID, String supplierName) {
-    this.supplierID = supplierID;
-    this.supplierName = supplierName;
-  }
+    // Constructor to connect to ApplicationFrame Data
+    public Supplier(int supplierID, String supplierName, String address, String phoneNumber, String emailAddress) {
+        this.supplierID = supplierID;
+        this.supplierName = supplierName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
 
-  //Getters and setters 
+    // Getters
+    public int getSupplierID() {
+        return this.supplierID;
+    }
 
-  public int getSupplierID() {
-    return this.supplierID;
-  }
+    public String getSupplierName() {
+        return this.supplierName;
+    }
 
-  public void setSupplierID(int supplierID) {
-    this.supplierID = supplierID;
-  }
+    public String getAddress() {
+        return this.address;
+    }
 
-  public String getSupplierName() {
-    return this.supplierName;
-  }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
 
-  public void setSupplierName(String supplierName) {
-    this.supplierName = supplierName;
-  }
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
+
+    // Setters
+    public void setSupplierID(int supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
 }
